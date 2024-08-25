68bbd23431958638e19f5245d15337b5237519fc
HamdiAyhan <hamdiminer1@gmail.com>
2024-08-25 18:54:17 +0300

    trtr

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { useRef , useEffect } from 'react';
+
 
 function App() {
+  const paragrafRef = useRef()
+  const sayacREf = useRef()
+  function artır (){
+sayacREf.current = sayacREf.current +5
+alert(sayacREf.current)
+  }
   return (
     <div className="App">
-
+<p ref={paragrafRef} >Test</p>
+<button onClick={artır}>Artır</button>
     </div>
   );
 }
